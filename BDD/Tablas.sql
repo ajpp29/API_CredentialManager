ff21c79dbd5d08536f91e4c2a2ba499291a45964
Angel Jimenez <angeljpp29@gmail.com>
2024-12-30 00:54:43 -0600

    Update Tablas.sql

--- BDD/Tablas.sql
+++ BDD/Tablas.sql
@@ -11,40 +11,40 @@ CREATE TABLE dbo.SERVIDOR
 	ServidorID	 INT		  IDENTITY(1, 1) CONSTRAINT PK_SERVIDOR PRIMARY KEY,
 	Servidor	 VARCHAR(100) NOT NULL CONSTRAINT UQ_SERVIDOR_SERVIDOR UNIQUE,
 	ServidorIP	 VARCHAR(100) NOT NULL CONSTRAINT UQ_SERVIDOR_SERVIDORIP UNIQUE,
-	Fec_Creacion DATETIME	  CONSTRAINT DF_SERVIDOR_FEC_CREACION DEFAULT GETDATE(), -- Account creation date
-	Activo		 BIT		  CONSTRAINT DF_SERVIDOR_ACTIVO DEFAULT 1				 -- Flag to indicate if the account is active
+	Fec_Creacion DATETIME	  CONSTRAINT DF_SERVIDOR_FEC_CREACION DEFAULT GETDATE(), 
+	Activo		 BIT		  CONSTRAINT DF_SERVIDOR_ACTIVO DEFAULT 1				 
 );
 
 CREATE TABLE dbo.USUARIO
 (
-	UsuarioID		INT			  IDENTITY(1, 1) CONSTRAINT PK_USUARIO PRIMARY KEY,		-- Unique user ID
-	Usuario			NVARCHAR(50)  NOT NULL CONSTRAINT UQ_USUARIO_USUARIO UNIQUE,		-- Username (must be unique)
-	Correo			NVARCHAR(100) NOT NULL CONSTRAINT UQ_USUARIO_CORREO UNIQUE,			-- Email address (must be unique)
-	Clave			NVARCHAR(256) NOT NULL,												-- Store hashed passwords
+	UsuarioID		INT			  IDENTITY(1, 1) CONSTRAINT PK_USUARIO PRIMARY KEY,		
+	Usuario			NVARCHAR(50)  NOT NULL CONSTRAINT UQ_USUARIO_USUARIO UNIQUE,		
+	Correo			NVARCHAR(100) NOT NULL CONSTRAINT UQ_USUARIO_CORREO UNIQUE,			
+	Clave			NVARCHAR(256) NOT NULL,												
 	LlaveEncripcion NVARCHAR(32)  NOT NULL,
-	Fec_Creacion	DATETIME	  CONSTRAINT DF_USUARIO_FEC_CREACION DEFAULT GETDATE(), -- Account creation date
-	Activo			BIT			  CONSTRAINT DF_USUARIO_ACTIVO DEFAULT 1				-- Flag to indicate if the account is active
+	Fec_Creacion	DATETIME	  CONSTRAINT DF_USUARIO_FEC_CREACION DEFAULT GETDATE(), 
+	Activo			BIT			  CONSTRAINT DF_USUARIO_ACTIVO DEFAULT 1				
 );
 
 CREATE TABLE dbo.CREDENCIAL
 (
-	CredencialID	 INT		   IDENTITY(1, 1) CONSTRAINT PK_CREDENCIAL PRIMARY KEY, -- Unique credential ID
-	UsuarioID		 INT		   NOT NULL,											-- Foreign key to Users
+	CredencialID	 INT		   IDENTITY(1, 1) CONSTRAINT PK_CREDENCIAL PRIMARY KEY, 
+	UsuarioID		 INT		   NOT NULL,											
 	ServidorID		 INT		   NOT NULL,
-	CredencialNombre NVARCHAR(100) NOT NULL,											-- Name/description of the credential (e.g., "Gmail Account")
-	Usuario			 NVARCHAR(100) NOT NULL,											-- Username for the credential
-	Fec_Creacion	 DATETIME	   DEFAULT GETDATE(),									-- When the credential was created
-	Activo			 BIT		   DEFAULT 1,											-- Flag to indicate if the credential is active
+	CredencialNombre NVARCHAR(100) NOT NULL,											
+	Usuario			 NVARCHAR(100) NOT NULL,											
+	Fec_Creacion	 DATETIME	   DEFAULT GETDATE(),									
+	Activo			 BIT		   DEFAULT 1,											
 	CONSTRAINT FK_CREDENCIAL_USUARIO FOREIGN KEY (UsuarioID) REFERENCES SecretManager.dbo.USUARIO (UsuarioID) ON DELETE CASCADE,
 	CONSTRAINT FK_CREDENCIAL_SERVIDOR FOREIGN KEY (ServidorID) REFERENCES SecretManager.dbo.SERVIDOR (ServidorID) ON DELETE CASCADE
 );
 
 CREATE TABLE dbo.CREDENCIAL_HISTORIAL
 (
-	CredencialHistorialID INT			IDENTITY(1, 1) CONSTRAINT PK_CREDENCIAL_HISTORIAL PRIMARY KEY,		  -- Unique ID for each history record
-	CredencialID		  INT			NOT NULL,															  -- Foreign key to Credentials
-	CredencialClave		  NVARCHAR(256) NOT NULL,															  -- Hashed password
-	Fec_Transaccion		  DATETIME		CONSTRAINT DF_CREDENCIAL_HISTORIAL_FEC_TRANSACCION DEFAULT GETDATE(), -- Date and time of the password change
+	CredencialHistorialID INT			IDENTITY(1, 1) CONSTRAINT PK_CREDENCIAL_HISTORIAL PRIMARY KEY,		 
+	CredencialID		  INT			NOT NULL,															 
+	CredencialClave		  NVARCHAR(256) NOT NULL,															 
+	Fec_Transaccion		  DATETIME		CONSTRAINT DF_CREDENCIAL_HISTORIAL_FEC_TRANSACCION DEFAULT GETDATE(),
 	CONSTRAINT FK_CREDENCIAL_HISTORIAL_CREDENCIAL FOREIGN KEY (CredencialID) REFERENCES SecretManager.dbo.CREDENCIAL
 												  (CredencialID) ON DELETE CASCADE
 );
