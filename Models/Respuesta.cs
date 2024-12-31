1506ba2e072d9010050008062a248fa416d09281
Angel Jimenez <angeljpp29@gmail.com>
2024-12-31 00:37:32 -0600

    Creacion de modelo para respuesta

---
+++ Models/Respuesta.cs
@@ -0,0 +1,37 @@
+﻿namespace API_CredentialManager.Models
+{
+    public class Respuesta<T>
+    {
+        public int Codigo { get; set; }
+        public bool Exito { get; set; }
+        public string Mensaje { get; set; }
+        public T Dato { get; set; }
+
+        public Respuesta() 
+        {
+            Codigo = 200;
+            Exito = true;
+        }
+
+        public Respuesta(int codigo, bool exito)
+        {
+            Codigo = codigo;
+            Exito = exito;
+        }
+
+        public Respuesta(int codigo, bool exito, string mensaje)
+        {
+            Codigo = codigo;
+            Exito = exito;
+            Mensaje = mensaje;
+        }
+
+        public Respuesta(int codigo, bool exito, string mensaje, T dato)
+        {
+            Codigo = codigo;
+            Exito = exito;
+            Mensaje = mensaje;
+            Dato = dato;
+        }
+    }
+}
